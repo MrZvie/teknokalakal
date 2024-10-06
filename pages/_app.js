e27d2e79312aae1182e999720257153590784895
MrZvie <ddy010718@gmail.com>
2024-10-06 21:21:00 +0800

    changing the title and the icon that showin in the browser

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,17 @@
 import "@/styles/globals.css";
 import { SessionProvider } from "next-auth/react"
+import Head from "next/head";
 
 export default function App({Component, pageProps: { session, ...pageProps }}) 
 {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps}/>
+      <Head>
+        <title>TeknoKalkal-Admin</title>
+        <meta name="description" content="My app description" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
     </SessionProvider>
-  )
+  );
 }
