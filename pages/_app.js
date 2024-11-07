69d34d511946730348df9c0ea6c11b300eb88eb0
MrZvie <ddy010718@gmail.com>
2024-11-07 12:50:45 +0800

    updated files

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,7 @@ export default function App({Component, pageProps: { session, ...pageProps }})
     <SessionProvider session={session}>
       <Head>
         <title>TeknoKalkal-Admin</title>
-        <meta name="description" content="My app description" />
+        <meta name="description" content="This is the admin panel for TeknoKalkal" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Component {...pageProps} />
